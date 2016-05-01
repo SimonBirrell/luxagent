7a1e6bb4001d88427a226fbbe5db4f77e4daaa2d
Simon Birrell <simon@robotlux.com>
2016-05-01 10:39:49 +0200

    Try install_requires

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['luxagent'],
     package_dir={'': 'src'},
+    install_requires=['rosbridge_library']
 )
 
 setup(**d)
